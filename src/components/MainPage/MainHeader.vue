b4ec253ec0b5e7026cbd0765888098e143d66d5c
MalwinaS <malwinastepien93@gmail.com>
2023-03-02 18:29:01 +0100

    create scss

---
+++ src/components/MainPage/MainHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <picture>
+
+    </picture>
+</template>
+
+<script>
+export default Vue.extend({
+  name: 'MainHeader',
+  components: {
+    
+  },
+  props: {
+
+  },
+  computed: {
+
+    },
+});
+
+</script>
+
+
+<style scoped>
+
+
+</style>
