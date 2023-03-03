a94bca5ff67494af1d4a814bfec8741f4671f6d9
MalwinaS <malwinastepien93@gmail.com>
2023-03-03 21:32:11 +0100

    views, router, scss

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,46 @@
 <template>
-  <MainPage />
+  <div class="logo-image">
+    <img alt="Vue logo" src="./assets/images/header/logo.png">
+  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">O nas</router-link> |
+    <router-link to="/store">Sklep</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import MainPage from './views/MainPage.vue'
-
-export default {
-  name: 'App',
-  components: {
-    MainPage
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $font-family-base;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $color-primary;
+}
+
+.logo-image {
+  padding: 20px;
+  img {
+    @include devices(tablet) {
+      padding: 0;
+      width: 220px;
+      height: auto;
+    }
+  }
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: $color-primary;
+  font-size: $font-size-base;
+}
+
+nav a.router-link-exact-active {
+  color: $color-green;
 }
 </style>
